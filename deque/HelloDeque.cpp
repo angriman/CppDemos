324bbdfd456446cff82de562d8a5a0133e5aaeb8
Eugenio Angriman <eangriman@uni-koeln.de>
2017-12-05 23:32:09 +0100

    Removed comments

--- deque/HelloDeque.cpp
+++ deque/HelloDeque.cpp
@@ -16,28 +16,28 @@ int main() {
     std::deque<int> deque(10, 0);
 
     printDequeContent(deque);
-    //
-    // // Inserting elements in the deque
-    // for (int i = 0; i < deque.size(); ++i) {
-    //     deque[i] = i + 1;
-    // }
-    //
-    // printDequeContent(deque);
-    //
-    //
-    // // Pushing new elements in the queue
-    // int newElements = 10;
-    // for (int i = 0; i < newElements; ++i) {
-    //     // Even number
-    //     if (i % 2 == 0) {
-    //         deque.push_front(i);
-    //     }
-    //     else {
-    //         deque.push_back(i);
-    //     }
-    // }
-    //
-    // printDequeContent(deque);
+    
+    // Inserting elements in the deque
+    for (int i = 0; i < deque.size(); ++i) {
+        deque[i] = i + 1;
+    }
+
+    printDequeContent(deque);
+
+
+    // Pushing new elements in the queue
+    int newElements = 10;
+    for (int i = 0; i < newElements; ++i) {
+        // Even number
+        if (i % 2 == 0) {
+            deque.push_front(i);
+        }
+        else {
+            deque.push_back(i);
+        }
+    }
+
+    printDequeContent(deque);
 
     return 0;
 }
