46c70a640c6982e73bcfec3dc52e598ebb5d2be3
Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>
2018-10-30 12:27:29 +0100

    Formatter adaptors2 + using const reference
    Signed-off-by: Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>

--- ContainerAdaptors/Adaptors2.cpp
+++ ContainerAdaptors/Adaptors2.cpp
@@ -1,27 +1,27 @@
+#include <iostream>
 #include <stack>
 #include <vector>
-#include <iostream>
 
-void printVector(std::vector<int> v) {
-    std::cout << "{ ";
-    for (int i : v) {
-        std::cout << i << " ";
-    }
-    std::cout << "}\n";
+void printVector(const std::vector<int> &v) {
+	std::cout << "{ ";
+	for (int i : v) {
+		std::cout << i << " ";
+	}
+	std::cout << "}\n";
 }
 
 int main() {
-    std::stack<std::vector<int>, std::vector<std::vector<int>>> stack;
+	std::stack<std::vector<int>, std::vector<std::vector<int>>> stack;
 
-    std::vector<int> first = {0,1,2};
-    std::vector<int> second = {3,4,5};
+	std::vector<int> first = {0, 1, 2};
+	std::vector<int> second = {3, 4, 5};
 
-    stack.push(first);
-    stack.push(second);
+	stack.push(first);
+	stack.push(second);
 
-    std::cout << "Size of the stack " << stack.size() << '\n';
+	std::cout << "Size of the stack " << stack.size() << '\n';
 
-    std::cout << "Top element of the stack = " ;
-    printVector(stack.top());
-    return 0;
+	std::cout << "Top element of the stack = ";
+	printVector(stack.top());
+	return 0;
 }
