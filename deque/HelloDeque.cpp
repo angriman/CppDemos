bcc3f109c2413f02b9db9100287e901edd8fe8ad
Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>
2018-10-30 11:48:53 +0100

    Formatted hellodeque
    Signed-off-by: Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>

--- deque/HelloDeque.cpp
+++ deque/HelloDeque.cpp
@@ -3,40 +3,39 @@
 
 // Function that prints the content of the deque
 void printDequeContent(std::deque<int> d) {
-    std::cout << "Deque content: ";
-    for (int i = 0; i < d.size(); ++i) {
-        std::cout << d[i] << " ";
-    }
-    std::cout << '\n';
+	std::cout << "Deque content: ";
+	for (int i = 0; i < d.size(); ++i) {
+		std::cout << d[i] << " ";
+	}
+	std::cout << '\n';
 }
 
 int main() {
 
-    // 10 zero-initialized elements
-    std::deque<int> deque(10, 0);
+	// 10 zero-initialized elements
+	std::deque<int> deque(10, 0);
 
-    printDequeContent(deque);
+	printDequeContent(deque);
 
-    // Inserting elements in the deque
-    for (int i = 0; i < deque.size(); ++i) {
-        deque[i] = i + 1;
-    }
+	// Inserting elements in the deque
+	for (int i = 0; i < deque.size(); ++i) {
+		deque[i] = i + 1;
+	}
 
-    printDequeContent(deque);
+	printDequeContent(deque);
 
-    // Pushing new elements in the queue
-    int newElements = 10;
-    for (int i = 0; i < newElements; ++i) {
-        // Even number
-        if (i % 2 == 0) {
-            deque.push_front(i);
-        }
-        else {
-            deque.push_back(i);
-        }
-    }
+	// Pushing new elements in the queue
+	int newElements = 10;
+	for (int i = 0; i < newElements; ++i) {
+		// Even number
+		if (i % 2 == 0) {
+			deque.push_front(i);
+		} else {
+			deque.push_back(i);
+		}
+	}
 
-    printDequeContent(deque);
+	printDequeContent(deque);
 
-    return 0;
+	return 0;
 }
