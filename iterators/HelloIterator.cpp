949be42608add827d791405984f085df9efda9f3
Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>
2018-10-30 12:00:30 +0100

    Formatted helloiterator
    Signed-off-by: Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>

--- iterators/HelloIterator.cpp
+++ iterators/HelloIterator.cpp
@@ -1,30 +1,30 @@
-#include <vector>
 #include <iostream>
+#include <vector>
 
 int main() {
 
-    // Creating a new STL vector
-    std::vector<double> v({1,2,3,4,5});
+	// Creating a new STL vector
+	std::vector<double> v({1, 2, 3, 4, 5});
 
-    // Iterator pointing at the FIRST element of v
-    std::vector<double>::iterator iter = v.begin();
+	// Iterator pointing at the FIRST element of v
+	std::vector<double>::iterator iter = v.begin();
 
-    // Nice trick: use auto
-    auto lazyIter = v.begin();
+	// Nice trick (only from C++11): use auto
+	auto lazyIter = v.begin();
 
-    // Printing the value at which iter is pointing to
-    std::cout << "\nMy iterator points to " << *iter << "\n\n";
+	// Printing the value at which iter is pointing to
+	std::cout << "\nMy iterator points to " << *iter << "\n\n";
 
-    // Incrementing the pointer
-    ++iter;
-    std::cout << "After ++ my iterator points to " << *iter << "\n\n";
+	// Incrementing the pointer
+	++iter;
+	std::cout << "After ++ my iterator points to " << *iter << "\n\n";
 
-    iter--;
+	--iter;
 
-    // Using pointer to iterate
-    for (; iter != v.end(); ++iter) {
-        std::cout << "Item " << *iter << '\n';
-    }
+	// Using pointer to iterate
+	for (; iter != v.end(); ++iter) {
+		std::cout << "Item " << *iter << '\n';
+	}
 
-    return 0;
+	return 0;
 }
