9ac97dd05386c4c2f76375005acf69ac99f0b653
Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>
2018-10-30 12:20:51 +0100

    Formatted adaptors
    Signed-off-by: Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>

--- ContainerAdaptors/Adaptors.cpp
+++ ContainerAdaptors/Adaptors.cpp
@@ -1,27 +1,28 @@
+#include <iostream>
 #include <stack>
 #include <vector>
-#include <iostream>
 
 void printVector(std::vector<int> v) {
-    std::cout << "{ ";
-    for (int i : v) {
-        std::cout << i << " ";
-    }
-    std::cout << "}\n";
+	std::cout << "{ ";
+	for (int i : v) {
+		std::cout << i << " ";
+	}
+	std::cout << "}\n";
 }
 
 int main() {
-    std::stack<std::vector<int>> stack;
 
-    std::vector<int> first = {0,1,2};
-    std::vector<int> second = {3,4,5};
+	std::stack<std::vector<int>> stack;
 
-    stack.push(first);
-    stack.push(second);
+	std::vector<int> first = {0, 1, 2};
+	std::vector<int> second = {3, 4, 5};
 
-    std::cout << "Size of the stack " << stack.size() << '\n';
+	stack.push(first);
+	stack.push(second);
 
-    std::cout << "Top element of the stack = " ;
-    printVector(stack.top());
-    return 0;
+	std::cout << "Size of the stack " << stack.size() << '\n';
+
+	std::cout << "Top element of the stack = ";
+	printVector(stack.top());
+	return 0;
 }
