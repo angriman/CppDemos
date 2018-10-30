879f1a5df9cd75334591bb90f45d067a6e0b29f6
Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>
2018-10-30 11:52:40 +0100

    Further comments
    Signed-off-by: Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>

--- deque/HelloDeque.cpp
+++ deque/HelloDeque.cpp
@@ -7,24 +7,25 @@ void printDequeContent(std::deque<int> d) {
 	for (int i = 0; i < d.size(); ++i) {
 		std::cout << d[i] << " ";
 	}
-	std::cout << '\n';
+	std::cout << std::endl;
 }
 
 int main() {
 
-	// 10 zero-initialized elements
+	// We use a fill constructor to fill the deque with 10 zeros.
 	std::deque<int> deque(10, 0);
 
+	// We use the function defined above to print the content of the deque.
 	printDequeContent(deque);
 
-	// Inserting elements in the deque
+	// Modifying the elements in the deque.
 	for (int i = 0; i < deque.size(); ++i) {
 		deque[i] = i + 1;
 	}
 
 	printDequeContent(deque);
 
-	// Pushing new elements in the queue
+	// Pushing new elements in the queue on both front and back.
 	int newElements = 10;
 	for (int i = 0; i < newElements; ++i) {
 		// Even number
